5750f76c61d4794753e2df22618d64c1340d27f6
mbartus <mack.bartus@gmail.com>
2015-01-23 15:29:56 -0600

    Initial

---
+++ goatMGMT/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace goatMGMT
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

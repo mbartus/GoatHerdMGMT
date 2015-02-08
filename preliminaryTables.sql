14156cb2d0cc23ec31dfeef8bc5de25241ec9687
Michael Pace <terinopace@gmail.com>
2015-02-08 15:06:56 -0600

    Update preliminaryTables.sql
    Reduced and simplified number of tables to be generic. Now we can account for ANY type of animal.

--- preliminaryTables.sql
+++ preliminaryTables.sql
@@ -1,16 +1,25 @@
 /*
 We need a list of possible breeds.
-What is Pregenancy check?
-What is Parity?
-What is Birthing Score?
-What is Kidding Date?
+What is Pregenancy check? 156 days, expected date, p
+What is Parity? What nth time is this
+What is Birthing Score? 
+What is Kidding Date? Date the kid is born
+
+Lambing date
+
+Status code
+- active, alive and in the herd, available for breeding
+- sold for me
+- sold for breeding
+- died
+- culled, no good, not healthy
 
 How long are kids supposed to be weaned? (60 days)
-Birthing Type?
-creep foster?
+Birthing Type? single, double, 
+creep foster? if mother dies, will give to another mother
 weaning group?
 status code?
-market weight versus 60 day weight?
+market weight, date they were sold versus 60 day weight?
 
 When is a kid not considered a goat/sire? We need to add functionality 
 to notice and transfer kids to sires/dams OR give the user the ability
@@ -20,34 +29,17 @@ What are group treatment groups?
 
 */
 
-DROP TABLE Income CASCADE;
-DROP TABLE Expenses CASCADE;
-DROP TABLE Purchases CASCADE;
-DROP TABLE Customers CASCADE;
 DROP TABLE Treatments CASCADE;
-DROP TABLE Postweanings CASCADE;
 DROP TABLE Breedings CASCADE;
+
 DROP TABLE Births CASCADE;
-DROP TABLE Kids CASCADE;
-DROP TABLE Sires CASCADE;
-DROP TABLE Dams CASCADE;
-DROP TABLE Goats CASCADE;
+DROP TABLE Children CASCADE;
+DROP TABLE Animals CASCADE;
 
-CREATE TABLE Income(
-	cid char(5) NOT NULL,
-	dop date,
-	itemdetail varchar(100),
-	itemtype varchar(100),
-	quantity decimal(18,4),
-	unitPrice decimal(18,2),
-	totalPrice decimal(18,2),
-	notes varchar(1000),
-	PRIMARY KEY (id) REFERENCES Customers(cid)
-)
-
-CREATE TABLE Expenses(
+CREATE TABLE Transaction(
 	pid char(5) NOT NULL,
 	dop date,
+	type char(1) -- i for income, e for expense
 	itemdetail varchar(100),
 	itemtype varchar(100),
 	quantity decimal(18,4),
@@ -57,123 +49,110 @@ CREATE TABLE Expenses(
 	PRIMARY KEY (id) REFERENCES Purchases(pid)
 )
 
-CREATE TABLE Purchases(
-	pid char(5) NOT NULL,
-	PRIMARY KEY (pid)
+CREATE TABLE Associates(
+	aid char(5) UNIQUE NOT NULL,
+	name text,
+	street text,
+	city text,
+	state text,
+	zip text,
+	telephone text,
+	fax text,
+	email text,
+	notes text,
+	PRIMARY KEY (aid)
 )
 
-CREATE TABLE Customers(
-	cid char(5) NOT NULL,
-	PRIMARY KEY (cid)
-)
-
-CREATE TABLE Treatments(
-	sid char(5) NOT NULL,
-	dot date,
+CREATE TABLE AnimalTreatment(
+	aid char(5) UNIQUE NOT NULL,
+	tid char(5) UNIQUE NOT NULL,
+	date_applied date,
 	details varchar(50),
 	product varchar(50),
 	dosage varchar(50),
 	remarks varchar(50)
-	PRIMARY KEY (aid),
-	FOREIGN KEY (aid) REFERENCES Animals(aid),
+	PRIMARY KEY (aid, tid, date_applied),
+	FOREIGN KEY (aid) REFERENCES Animals(id),
+	FOREIGN KEY (tid) REFERENCES Treatments(tid),
 )
 
-CREATE TABLE Postweanings(
-	kid char(5) NOT NULL,
-	weighdate date,
-	weight decimal(4,2),
-	/*
-	parity
-	creepfoster
-	weaninggroup
-	statuscode
-	*/
-	marketweight decimal(4,2),
-	PRIMARY KEY (kid, weighdate)
-	FOREIGN KEY (kid) REFERENCES Kids(kid)
+CREATE TABLE Treatments(
+	tid char(5) UNIQUE NOT NULL,
+	product text(30),
+	dosage text(30),
+	remarks text(30),
+	PRIMARY KEY (tid)
 )
 
 CREATE TABLE Breedings(
-	sid char(5) NOT NULL,
+	id char(5) NOT NULL,
 	mid char(5) NOT NULL,
-	mdate date,
-	PRIMARY KEY (sid, mid),
-	FOREIGN KEY (sid) REFERENCES Sires(sid),
-	FOREIGN KEY (mid) REFERENCES Dams(mid)
+	fid char(5) NOT NULL,
+	mating_date date,
+	remarks text(1000)
+	PRIMARY KEY (id, eid, rid),
+	FOREIGN KEY (mid) REFERENCES Animals(id),
+	FOREIGN KEY (fid) REFERENCES Animals(id),
 )
 
 CREATE TABLE Births(
-	kid char(5) NOT NULL,
-	sid char(5) NOT NULL,
+	id char(5) NOT NULL,
 	mid char(5) NOT NULL,
-	mdate date, 
-	-- add birthing type here
-	PRIMARY KEY (kid, sid, mid),
-	FOREIGN KEY (kid) REFERENCES Dams(kid)
-	FOREIGN KEY (sid) REFERENCES Sires(sid),
-	FOREIGN KEY (mid) REFERENCES Dams(mid)
+	fid char(5) NOT NULL,
+	birth_type int(2),
+	birth_parity int(2),
+	remarks text(1000)
+	PRIMARY KEY (id, did, bid),
+	FOREIGN KEY (mid) REFERENCES Animals(id),
+	FOREIGN KEY (fid) REFERENCES Animals(id),
 )
 
-CREATE TABLE Kids(
-	kid char(5) NOT NULL,
-	reg char(5) NOT NULL,
-	registry varchar(50),
+CREATE TABLE Children(
+	species char(15),
+	id char(15) UNIQUE NOT NULL,
+	dob date NOT NULL,
+	farm_name varchar(50) NOT NULL,
 	name varchar(50),
-	breed varchar(50),
-	dob date,
-	weight decimal(4,2)
-	sex char(1), --M for male, F for female
-	PRIMARY KEY (kid, reg),
-	FOREIGN KEY (kid, reg) REFERENCES Animals(aid, reg),
-)
-
-CREATE TABLE Sires(
-	sid char(5) NOT NULL,
-	reg char(5) NOT NULL,
-	registry varchar(50),
-	name varchar(50),
-	breed varchar(50),
-	dob date,
-	weight decimal(4,2)
-	sex char(1), --M for male, F for female
-	PRIMARY KEY (sid, reg),
-	FOREIGN KEY (sid, reg) REFERENCES Animals(aid, reg)
-)
-
-CREATE TABLE Dams(
-	did char(5) NOT NULL,
-	reg char(5) NOT NULL,
-	registry varchar(50),
-	name varchar(50),
-	breed varchar(50),
-	dob date,
-	weight decimal(4,2)
-	sex char(1), --M for male, F for female
-	PRIMARY KEY (did, reg),
-	FOREIGN KEY (did, reg) REFERENCES Animals(aid, reg)
-)
-
-CREATE TABLE Goats(
-	gid char(5) NOT NULL,
-	reg char(5) NOT NULL,
-	registry varchar(50),
-	name varchar(50),
-	breed varchar(50),
-	dob date,
-	weight decimal(4,2)
-	sex char(1), --M for male, F for female
-	PRIMARY KEY (gid, reg),
-	FOREIGN KEY (gid, reg) REFERENCES Animals(aid, reg)
+	regulation_no char(15) UNIQUE,
+	breed_code char(5) NOT NULL,
+	microchip_id varchar(15),
+	premise_id varchar(15), -- what is this?
+	herd_id_code varchar(15), -- what is this?
+	breed_registry varchar(15), -- what is this?
+	sex char(1), -- M for male, F for female
+	status_code varchar(10),
+	disposal date, -- the day the animal dies?
+	current_weight decimal(4,2),
+	weight_date date,
+	-- Birth, need to figure out creep foster
+	weaning_weight int,
+	birth_weight decimal(4,2),
+	weaning_weight decimal(4,2),
+	-- Market
+	market_weight decimal(4,2),
+	market_date date,
+	remarks text(1000)
+	PRIMARY KEY (id, dob) --PRIMARY KEY (id, farm_name, dob)
 )
 
 CREATE TABLE Animals(
-	aid char(5) NOT NULL,
-	reg char(5) NOT NULL,
-	registry varchar(50),
+	species char(15),
+	id char(15) UNIQUE NOT NULL,
+	dob date NOT NULL,
+	farm_name varchar(50) NOT NULL,
 	name varchar(50),
-	breed varchar(50),
-	dob date,
-	weight decimal(4,2)
-	sex char(1), --M for male, F for female
-	PRIMARY KEY (aid, reg)
+	regulation_no char(15) UNIQUE,
+	breed_code char(5) NOT NULL,
+	microchip_id varchar(15),
+	premise_id varchar(15), -- what is this?
+	herd_id_code varchar(15), -- what is this?
+	breed_registry varchar(15), -- what is this?
+	sex char(1), -- M for male, F for female
+	status_code varchar(10),
+	disposal date, -- the day the animal dies?
+	current_weight decimal(4,2),
+	market_weight decimal(4,2),
+	market_date date,
+	remarks text(1000)
+	PRIMARY KEY (id, dob) --PRIMARY KEY (id, farm_name, dob)
 )

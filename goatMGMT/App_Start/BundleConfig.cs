06358cf2cc408aeaae678a9633e0b90720bb5747
mbartus <mack.bartus@gmail.com>
2015-04-10 11:17:09 -0500

    Updated everything and also made quick fix to IE

--- goatMGMT/App_Start/BundleConfig.cs
+++ goatMGMT/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace goatMGMT
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.migrate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
